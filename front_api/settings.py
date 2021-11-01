cef867e199267a884818c2ba1db870d07aa9154f
chiyoung0011 <jcy0011@naver.com>
2021-11-01 17:15:21 +0900

    db connected to aws mysql server

--- front_api/settings.py
+++ front_api/settings.py
@@ -85,8 +85,15 @@ WSGI_APPLICATION = 'front_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+        }
     }
 }
 
